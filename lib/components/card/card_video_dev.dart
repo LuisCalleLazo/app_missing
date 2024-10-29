3ca3a00f2975cf601856aeb3e39a03d04230b006
Luis Calle Lazo <callelazodeynarluis@gmail.com>
2024-10-29 00:15:44 -0400

    card video

--- lib/components/card/card_video_dev.dart
+++ lib/components/card/card_video_dev.dart
@@ -3,49 +3,56 @@ import 'package:flutter/material.dart';
 class CardVideoDev extends StatelessWidget {
   const CardVideoDev({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Card(
       clipBehavior: Clip.hardEdge,
       elevation: 10,
-      child: Column(
-        children: [
-          Image.network(
-            'https://picsum.photos/id/1/600/250',
-            height: 250,
-            fit: BoxFit.cover,
-          ),
-          Container(
-            color: Colors.amber[600],
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+      child: Container(
+        height: 70,
+        color: Colors.blueGrey,
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
               children: [
-                IconButton(
-                  color: Colors.white,
-                  icon: const Icon(Icons.favorite),
-                  onPressed: () {
-                    // showDialog(
-                    //   context: context,
-                    //   builder: (BuildContext context) =>
-                    //       const DialogCalification(),
-                    // );
-                  },
+                Container(
+                  height: 50,
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    color: Colors.white,
+                    iconSize: 30,
+                    icon: const Icon(Icons.play_arrow),
+                    onPressed: () => {/* showSnackbarMessages(context) */},
+                  ),
                 ),
-                IconButton(
-                  color: Colors.white,
-                  icon: const Icon(Icons.comment),
-                  onPressed: () => {/* showSnackbarMessages(context) */},
+                const SizedBox(
+                  width: 20,
                 ),
-                IconButton(
-                  color: Colors.white,
-                  icon: const Icon(Icons.share_rounded),
-                  onPressed: () => {/* showSnackbarShared(context) */},
+                Container(
+                  color: Colors.transparent,
+                  width: 50,
+                  height: 50,
+                  child: const CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://indiehoy.com/wp-content/uploads/2018/04/aurora.jpg"),
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
+            const Text(
+              "2023 / 12/ 09",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            )
+          ],
+        ),
       ),
     );
   }
